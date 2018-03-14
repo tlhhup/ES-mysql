533e007dac19e129b328fc6d3ff1b656de34f108
tlhhup <137273278@qq.com>
2018-03-14 12:22:15 +0800

    1.添加high level rest api

--- src/test/java/org/tlh/em/test/ESTest.java
+++ src/test/java/org/tlh/em/test/ESTest.java
@@ -1,9 +1,14 @@
 package org.tlh.em.test;
 
-import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
 
 import org.apache.http.HttpHost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.nio.entity.NStringEntity;
+import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.Response;
+import org.elasticsearch.client.ResponseListener;
 import org.elasticsearch.client.RestClient;
 import org.junit.After;
 import org.junit.Before;
@@ -38,12 +43,36 @@ public class ESTest {
 	@Test
 	public void query() {
 		try {
-			Response response = restClient.performRequest("GET", "/");
-			
-			System.out.println(response.getEntity().toString());
-		} catch (IOException e) {
+			//同步调用
+			//Response response = restClient.performRequest("GET", "/");
+			//System.out.println(response.getEntity().toString());
+			//异步调用
+			restClient.performRequestAsync("GET", "/", new ResponseListener() {
+				
+				@Override
+				public void onSuccess(Response response) {
+					System.err.println(response);
+				}
+				
+				@Override
+				public void onFailure(Exception exception) {
+					exception.printStackTrace();
+				}
+			});
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	@Test
+	public void query1() throws Exception{
+		Map<String, String> params = Collections.emptyMap();
+		String jsonString = "{\"query\":{\"match\":{\"id\":\"1\"}}}";
+		NStringEntity entity = new NStringEntity(jsonString, ContentType.APPLICATION_JSON);
+		Response response = restClient.performRequest("GET", "/woniuxy/_search",params,entity);
+		
+		String string = EntityUtils.toString(response.getEntity());
+		System.out.println(string);
 	}
 
 }
